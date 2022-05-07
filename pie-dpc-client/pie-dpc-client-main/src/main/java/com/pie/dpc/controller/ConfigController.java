8329e836fdd0aa96b05117d818df1bc4ded3fa89
lucifer <wangxiyue.xy@163.com>
2022-05-07 10:38:51 +0800

    修正pom 映入notify 方式 test --> compile

--- pie-dpc-client/pie-dpc-client-main/src/main/java/com/pie/dpc/controller/ConfigController.java
+++ pie-dpc-client/pie-dpc-client-main/src/main/java/com/pie/dpc/controller/ConfigController.java
@@ -3,7 +3,10 @@ package com.pie.dpc.controller;
 /**
  * @Author wangxiyue.xy@163.com
  * @Date 2022/5/7 10:27
- * @Description TODO : 接受配置信息
+ * @Description TODO : 接收服务端 配置信息
  **/
+
 public class ConfigController {
+
+
 }
