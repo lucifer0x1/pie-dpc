260ef32f9cdd359d019f8f6572220677ca459287
虫子 <wangxiyue.xy@163.com>
2022-05-10 00:08:22 +0800

    解压缩方式 加载 so文件

--- pie-dpc-client/pie-dpc-client-filelistener/src/main/java/com/pie/dpc/filelistener/lib/LoadLibraryConfig.java
+++ pie-dpc-client/pie-dpc-client-filelistener/src/main/java/com/pie/dpc/filelistener/lib/LoadLibraryConfig.java
@@ -1,7 +1,15 @@
 package com.pie.dpc.filelistener.lib;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 
 /**
  * ClassName LoadLibraryConfig
@@ -13,23 +21,48 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoadLibraryConfig {
 
-    static
-    {
+    Logger logger = LoggerFactory.getLogger(LoadLibraryConfig.class);
 
-        System.out.println(LoadLibraryConfig.class.getClassLoader()
-                .getResource("lib/libjnotify.so"));
+    private static final String LIB_JNOTIFY_TARGET_PATH = "lib/libnotify.so";
 
-        System.load(LoadLibraryConfig.class.getClassLoader()
-                .getResource("lib/libjnotify.so").getPath());
 
-        int res = nativeInit();
-        if (res != 0)
-        {
-            throw new RuntimeException("Error initializing fshook_inotify library. linux error code #" + res  + ", man errno for more info");
+
+
+    private String extractLibrary() {
+        try {
+            // Create temporary file
+            File file = File.createTempFile("libnotify", ".lib");
+            // In case it worked, we can extract lib
+            if (file.exists()) {
+                // First of all, let's show where do we plan to extract it
+                logger.debug("Temporary file: {}" , file.getAbsoluteFile().toPath());
+                // Now, we can get the lib file from JAR and put it inside temporary location
+                InputStream link = (getClass().getResourceAsStream(LIB_JNOTIFY_TARGET_PATH));
+                // We want to overwrite existing file. This is why we are using
+                // java.nio.file.StandardCopyOption.REPLACE_EXISTING
+                Files.copy(
+                        link,
+                        file.getAbsoluteFile().toPath(),
+                        java.nio.file.StandardCopyOption.REPLACE_EXISTING);
+                return file.getAbsoluteFile().toPath().toString();
+            }
+            // In case something goes wrong, we will simply return null
+            return null;
+        } catch (IOException e) {
+            // The same goes for exception - we are passing null back
+            logger.error("ext library error : {} ",e.getMessage());
+            return null;
         }
-
     }
 
-    private static native int nativeInit();
+    @PostConstruct
+    public void initLoad(){
+        String filePath = extractLibrary();
+        if(filePath!=null){
+            System.load(filePath);
+        }else{
+            logger.error("extractLibrary un success");
+        }
+    }
 
 }
